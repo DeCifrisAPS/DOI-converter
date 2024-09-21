7d0e401f6fe9dcfb043a15f9d23b76b23e51085e
coding <coding@unpx.net>
2024-09-21 17:48:08 +0200

    Add examples for data structure and py3qt5 app

---
+++ src/converter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Overview of the process
+1. Read TSV
+2. Sanity Check
+2a. Error Handling
+3. Compile to JSON format
+
+Maybe there will be configuration to sync typescript model with this converter
+"""
