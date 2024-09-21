2d6febed71dc904c287a15da18b415e9155a1c76
d4 <coding@unpx.net>
2024-09-21 23:06:38 +0200

    add first mdoc stub; outline flow, tools and docs

--- src/converter.py
+++ src/converter.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import csv
+import sys
 
 """
 Overview of the process
@@ -9,3 +11,20 @@ Overview of the process
 
 Maybe there will be configuration to sync typescript model with this converter
 """
+
+def read_raw_data(filename: str) -> list[str]:
+    """Read Tab-Separated Value file"""
+    with open(filename, newline='') as tsvfile:
+        return csv.reader(tsvfile, delimiter='\t', quotechar='"')
+
+def write_json_data(filename: str, data: list[str]) -> None:
+    """Write JSON file for website"""
+    with open(filename, 'w', encoding='utf-8') as jsonfile:
+        json.dump(data, jsonfile, ensure_ascii=False) # , indent=4)
+
+def main() -> int:
+    """Do stuff"""
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
